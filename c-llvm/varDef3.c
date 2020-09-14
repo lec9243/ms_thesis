68923cc10b44df03e3e4999f6f84e68cb04743f6
lec9243 <lec9243@gmail.com>
2020-09-14 10:54:14 -0400

    update code

--- c-llvm/varDef3.c
+++ c-llvm/varDef3.c
@@ -3,8 +3,8 @@
 int main() {
     int n1, n2, n3;
     n1 = 2;
-    n2 = 3;
+    n2 = n1;
     n3 = n1;
-    //printf("number of n2 is %d.", n2);
+    printf("number of n2 is %d.", n2);
     return 0;
 }
